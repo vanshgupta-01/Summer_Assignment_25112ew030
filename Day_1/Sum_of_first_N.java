e3831ecdebe191f6fd3225c7736cb7cf4cf4e29c
vanshgupta-01 <guptavansh1010@gmail.com>
2026-05-28 18:07:01 +0530

    program1

---
+++ Day_1/Sum_of_first_N.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sum_of_first_N {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+    }
+}
